df8a39be93ba1ed65e8a1e27bf1a0a6b1858f662
bigetti <upgrade81@mail.ru>
2024-01-31 12:35:50 +0300

    First try

---
+++ measurement/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import CreateSensorView, UpdateSensorView, AddMeasurementView, GetSensorListView, GetSensorInfoView
+
+urlpatterns = [
+    path('create-sensor/', CreateSensorView.as_view(), name='create_sensor'),
+    path('update-sensor/<int:pk>/', UpdateSensorView.as_view(), name='update_sensor'),
+    path('add-measurement/', AddMeasurementView.as_view(), name='add_measurement'),
+    path('get-sensor-list/', GetSensorListView.as_view(), name='get_sensor_list'),
+    path('get-sensor-info/<int:pk>/', GetSensorInfoView.as_view(), name='get_sensor_info'),
+    # Другие маршруты...
+]
+
---
+++ measurement/views.py
@@ -0,0 +1,39 @@
+from rest_framework import generics
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from .models import Measurement, Sensor
+from .serializers import MeasurementSerializer, SensorDetailSerializer
+
+# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
+# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+# Создать датчик
+#POST
+class CreateSensorView(generics.CreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+# Изменить датчик
+#UPDATE
+class UpdateSensorView(generics.UpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+# Добавить измерение
+#UPDATE
+class AddMeasurementView(generics.CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+# Получить список датчиков
+#GET
+class GetSensorListView(generics.ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+# Получить информацию по конкретному датчику
+#GET
+class GetSensorInfoView(generics.RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+    lookup_field = 'id'
