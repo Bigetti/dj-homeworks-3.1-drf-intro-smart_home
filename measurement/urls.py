3f80dc1b2b61bfa3e2afc8a8c58723c8f28469ac
bigetti <upgrade81@mail.ru>
2024-02-07 12:04:15 +0300

    complited after review

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import CreateSensorView, SensorDetailView, AddMeasurementView, GetSensorListView
+# from .views import CreateSensorView, SensorDetailView, AddMeasurementView, GetSensorListView
+from .views import SensorListGreateView, SensorDetailView, AddMeasurementView, DeleteSensorView
+
 
 urlpatterns = [
-    path('create-sensor/', CreateSensorView.as_view(), name='create_sensor'),
-   # path('update-sensor/<int:pk>/', UpdateSensorView.as_view(), name='update_sensor'),
+    path('create-sensor/', SensorListGreateView.as_view(), name='create_sensor'),
+    path('update-sensor/<int:id>/', SensorDetailView.as_view(), name='update_sensor'),
+
     path('add-measurement/', AddMeasurementView.as_view(), name='add_measurement'),
-    path('get-sensor-list/', GetSensorListView.as_view(), name='get_sensor_list'),
+    path('get-sensor-list/', SensorListGreateView.as_view(), name='get_sensor_list'),
     #path('get-sensor-info/<int:id>/', GetSensorInfoView.as_view(), name='get_sensor_info'),
-    path('sensor/<int:id>/', SensorDetailView.as_view(), name='sensor_detail'),
+    path('get-sensor-info/<int:id>/', SensorDetailView.as_view(), name='sensor_detail'),
+    path('delete-sensor/<int:id>/', DeleteSensorView.as_view(), name ='delete_sensor'),
     # Другие маршруты...
 ]
 
--- measurement/views.py
+++ measurement/views.py
@@ -1,19 +1,13 @@
 from rest_framework import generics
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Measurement, Sensor
 from .serializers import MeasurementSerializer, SensorDetailSerializer
 
 
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-
-# Создать датчик
-# POST
-class CreateSensorView(generics.CreateAPIView):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorDetailSerializer
-
 
 
 # Обновить датчик и получить информацию о конкретном датчике
@@ -24,12 +18,6 @@ class SensorDetailView(generics.RetrieveUpdateAPIView):
     lookup_field = 'id'
 
 
-# # Изменить датчик
-# # PUT
-# class UpdateSensorView(generics.UpdateAPIView):
-#     queryset = Sensor.objects.all()
-#     serializer_class = SensorDetailSerializer
-
 
 # Добавить измерение
 # POST
@@ -38,11 +26,23 @@ class AddMeasurementView(generics.CreateAPIView):
     serializer_class = MeasurementSerializer
 
 
-# Получить список датчиков
-# GET
-class GetSensorListView(generics.ListAPIView):
+
+# Получить список датчиков и создать новый датчик
+# GET (получение списка) и POST (создание)
+class SensorListGreateView(generics.ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorDetailSerializer
+
+
+class DeleteSensorView(generics.DestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+    lookup_field = 'id'
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "Sensor successfully deleted"}, status=status.HTTP_200_OK)
 
 
 # # Получить информацию по конкретному датчику
@@ -51,3 +51,26 @@ class GetSensorListView(generics.ListAPIView):
 #     queryset = Sensor.objects.all()
 #     serializer_class = SensorDetailSerializer
 #     lookup_field = 'id'
+
+
+# Создать датчик
+# # POST
+# class CreateSensorView(generics.CreateAPIView):
+#     queryset = Sensor.objects.all()
+#     serializer_class = SensorDetailSerializer
+
+
+#
+# # Получить список датчиков
+# # GET
+# class GetSensorListView(generics.ListAPIView):
+#     queryset = Sensor.objects.all()
+#     serializer_class = SensorDetailSerializer
+
+
+
+# # Изменить датчик
+# # PUT
+# class UpdateSensorView(generics.UpdateAPIView):
+#     queryset = Sensor.objects.all()
+#     serializer_class = SensorDetailSerializer
